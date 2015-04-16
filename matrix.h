1d4309334b745d6df6d57cf5578dc06f76024684
Apocaliptis <maxym.osadchy@gmail.com>
2015-04-16 12:08:02 +0300

    Add lightMYled function

---
+++ matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef enum
+{
+	WHITE	= ((u8)0x0),	/*0b 000*/
+	PINK	= ((u8)0x1),	/*0b 001*/
+	SKY	= ((u8)0x2),	/*0b 010*/
+	BLUE	= ((u8)0x3),	/*0b 011*/
+	YELLOW	= ((u8)0x4),	/*0b 100*/
+	RED	= ((u8)0x5),	/*0b 101*/
+	GREEN	= ((u8)0x6),	/*0b 110*/
+	OFF	= ((u8)0x7)	/*0b 111*/
+ }Colour;
+
+
+#endif /* MATRIX_H */ 
